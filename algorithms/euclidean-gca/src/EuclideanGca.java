cf5363a6f055735d035dd5dc91108430fc8dc32f
Sagar Baver <sagar.baver@autodesk.com>
2021-04-02 12:20:23 +0530

    (algo): Adds Euclidean algo for computing GCA of two numbers

---
+++ algorithms/euclidean-gca/src/EuclideanGca.java
@@ -0,0 +1,66 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class EuclideanGca {
+
+  public static long gcd(long a, long b) {
+    if (a == 0) {
+      return b;
+    }
+
+    return gcd(b % a, a);
+  }
+
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+
+    long a = sc.nextLong();
+    long b = sc.nextLong();
+
+    out.println(gcd(a, b));
+
+    out.close();
+  }
+  // -----------PrintWriter for faster output---------------------------------
+  public static PrintWriter out;
+
+  // -----------MyScanner class for faster input----------
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+  // --------------------------------------------------------
+
+}
