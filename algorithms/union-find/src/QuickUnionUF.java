34a1e6ace1cc1ca0eaf2ee59c77b9c1f4c29126d
Sagar Baver <sagar.baver@autodesk.com>
2021-07-10 12:58:29 +0530

    (algo): Adds QuickUnionUF algo (refactor): Moves I/O ops into a utility class

--- algorithms/union-find/src/QuickUnionUF.java
+++ algorithms/union-find/src/QuickUnionUF.java
@@ -1,23 +1,7 @@
-/*
-Sample input:
-10
-10
-U 4 3
-U 3 8
-U 6 5
-U 9 4
-U 2 1
-F 8 9
-F 5 0
-U 5 0
-U 7 2
-U 6 1
- */
-
-public class QuickFindUF {
+public class QuickUnionUF {
   private final int[] id;
 
-  public QuickFindUF(int N) {
+  public QuickUnionUF(int N) {
     id = new int[N];
 
     for (int i = 0; i < N; i += 1) {
@@ -25,21 +9,25 @@ public class QuickFindUF {
     }
   }
 
-  // Quick find O(1)
-  public boolean isConnected(int p, int q) {
-    return id[p] == id[q];
+  private int root(int i) {
+    while (i != id[i]) {
+      i = id[i];
+    }
+
+    return i;
   }
 
-  // O(N^2)
-  public void union(int p, int q) {
-    int pid = id[p];
-    int qid = id[q];
+  // O(N)
+  public boolean isConnected(int p, int q) {
+    return root(p) == root(q);
+  }
 
-    for (int i = 0; i < id.length; i += 1) {
-      if (id[i] == pid) {
-        id[i] = qid;
-      }
-    }
+  // O(N)
+  public void union(int p, int q) {
+    int i = root(p);
+    int j = root(q);
+
+    id[i] = j;
   }
 
   public static void main(String[] args) {
@@ -56,7 +44,7 @@ public class QuickFindUF {
 
     System.out.println(
         "Enter either a Union operation (U P Q) or a Find operation (F P Q) for each test case");
-    QuickFindUF quickFindUF = new QuickFindUF(N);
+    QuickUnionUF quickUnionUF = new QuickUnionUF(N);
 
     while (T-- > 0) {
       operation = scanner.nextLine();
@@ -65,10 +53,10 @@ public class QuickFindUF {
       Q = Integer.parseInt(operation.split(" ")[2]);
 
       if (operation.split(" ")[0].equalsIgnoreCase("u")) {
-        quickFindUF.union(P, Q);
+        quickUnionUF.union(P, Q);
       } else {
         System.out.println();
-        System.out.printf("Are %s and %s connected: %s", P, Q, quickFindUF.isConnected(P, Q));
+        System.out.printf("Are %s and %s connected: %s", P, Q, quickUnionUF.isConnected(P, Q));
       }
     }
   }
---
+++ utils/src/InputOutput.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class InputOutput {
+  BufferedReader br;
+  StringTokenizer st;
+
+  public static PrintWriter out;
+
+
+  public InputOutput() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  String nextLine() {
+    String str = "";
+    try {
+      str = br.readLine();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return str;
+  }
+}
